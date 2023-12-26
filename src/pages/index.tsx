9be94a149ff5657fadcdc7f3cf62c8c3554e33fb
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2023-12-26 13:56:43 +0700

    feat: Update header meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from "next/head";
 import Footer from "@/components/footer";
 import Dashboard from "@/components/dashboard";
+import SEO from "@/components/SEO";
 
 export default function Main() {
   return (
     <>
-      <Head>
-        <title>ATAS Indonesia</title>
-        <meta name="description" content="Laman Resmi" />
-        <link rel="icon" href="/pavicon.io" />
-      </Head>
+      <SEO />
 
       <Dashboard />
 
