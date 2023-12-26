79269ffba05b4437b0e4ea47bc4b0091b65e53f1
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2023-12-26 13:43:30 +0700

    feat: Add dashboard css to app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
+import "../styles/dashboard.css";
 import { outfit } from "@/styles/fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
