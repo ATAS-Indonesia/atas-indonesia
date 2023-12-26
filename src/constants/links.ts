b34f51bce524280e1d42f14c00e3447c2b71abe7
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2023-12-26 13:53:33 +0700

    feat: Add link constants

---
+++ src/constants/links.ts
@@ -0,0 +1,11 @@
+// External links
+export const YOUTUBE_LINK = "https://www.youtube.com";
+export const INSTAGRAM_LINK = "https://www.instagram.com/atasindonesia";
+export const FACEBOOK_LINK = "https://www.facebook.com/atasindonesia";
+export const TWITTER_LINK = "https://www.twitter.com/atasindonesia";
+export const TIKTOK_LINK = "https://www.tiktok.com/@atasindonesia";
+
+// Internal links
+export const QNA_LINK = "https://bit.ly/QnAATAS";
+export const NOMOR_LINK = "https://bit.ly/NomorATAS";
+export const FORM_LINK = "https://forms.gle/QzM8wNqmfAXoi2718";
