b438daeb0782aff6c78b37bf26dc66f7970344bf
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-03-04 09:15:38 +0700

    feat: Update testimony

--- src/components/testimony.tsx
+++ src/components/testimony.tsx
@@ -27,63 +27,61 @@ function ItemCarousel(props: any) {
 
 function TestimonySection() {
   return (
-    <>
-      <div className="testimonyWrapper">
-        <div className="testimonyBox">
-          <img src="/images/petik.svg" alt="petik" />
-          <h2>Apa kata mereka?</h2>
-          <p>Ini yang mereka katakan setelah bergabung dengan ATAS Indonesia</p>
-        </div>
-
-        <OwlCarousel className="owl-theme" loop margin={10} nav>
-          <ItemCarousel
-            textTestimony="“ Wah, pokoknya kalian para garuda wajib banget gabung ATAS. Aku ikut seminar luar negeri tahun 2022 karena dapat informasi dari temen-temen ATAS. ”"
-            imgUser="/images/user1.svg"
-            imgAlt="user1"
-            name="Chandra Dwi Cahya"
-            desc="Pramuka Garuda Jawa Tengah"
-          />
-          <ItemCarousel
-            textTestimony="“ Percaya gak sih, aku lolos masuk IPB karena dibantu dengan ATAS. Penasaran kan kalian? Cepet-cepet gabung deh!. ”"
-            imgUser="/images/user2.svg"
-            imgAlt="user2"
-            name="Kartika Cinta"
-            desc="Pramuka Garuda Jawa Barat"
-          />
-          <ItemCarousel
-            textTestimony="“ Saya gak nyangka banget dapat undangan untuk jadi pembicara di SMP saya dulu hanya karena saya adalah anggota ATAS. ”"
-            imgUser="/images/user3.svg"
-            imgAlt="user3"
-            name="Bli Made"
-            desc="Pramuka Garuda Bali"
-          />
-
-          {/* borderline */}
-
-          <ItemCarousel
-            textTestimony="“ Wah, pokoknya kalian para garuda wajib banget gabung ATAS. Aku ikut seminar luar negeri tahun 2022 karena dapat informasi dari temen-temen ATAS. ”"
-            imgUser="/images/user1.svg"
-            imgAlt="user1"
-            name="Chandra Dwi Cahya"
-            desc="Pramuka Garuda Jawa Tengah"
-          />
-          <ItemCarousel
-            textTestimony="“ Percaya gak sih, aku lolos masuk IPB karena dibantu dengan ATAS. Penasaran kan kalian? Cepet-cepet gabung deh!. ”"
-            imgUser="/images/user2.svg"
-            imgAlt="user2"
-            name="Kartika Cinta"
-            desc="Pramuka Garuda Jawa Barat"
-          />
-          <ItemCarousel
-            textTestimony="“ Saya gak nyangka banget dapat undangan untuk jadi pembicara di SMP saya dulu hanya karena saya adalah anggota ATAS. ”"
-            imgUser="/images/user3.svg"
-            imgAlt="user3"
-            name="Bli Made"
-            desc="Pramuka Garuda Bali"
-          />
-        </OwlCarousel>
+    <div className="testimonyWrapper">
+      <div className="testimonyBox">
+        <img src="/images/petik.svg" alt="petik" />
+        <h2>Apa kata mereka?</h2>
+        <p>Ini yang mereka katakan setelah bergabung dengan ATAS Indonesia</p>
       </div>
-    </>
+
+      <OwlCarousel className="owl-theme" loop margin={10} nav>
+        <ItemCarousel
+          textTestimony="“ Wah, pokoknya kalian para garuda wajib banget gabung ATAS. Aku ikut seminar luar negeri tahun 2022 karena dapat informasi dari temen-temen ATAS. ”"
+          imgUser="/images/user1.svg"
+          imgAlt="user1"
+          name="Chandra Dwi Cahya"
+          desc="Pramuka Garuda Jawa Tengah"
+        />
+        <ItemCarousel
+          textTestimony="“ Percaya gak sih, aku lolos masuk IPB karena dibantu dengan ATAS. Penasaran kan kalian? Cepet-cepet gabung deh!. ”"
+          imgUser="/images/user2.svg"
+          imgAlt="user2"
+          name="Kartika Cinta"
+          desc="Pramuka Garuda Jawa Barat"
+        />
+        <ItemCarousel
+          textTestimony="“ Saya gak nyangka banget dapat undangan untuk jadi pembicara di SMP saya dulu hanya karena saya adalah anggota ATAS. ”"
+          imgUser="/images/user3.svg"
+          imgAlt="user3"
+          name="Bli Made"
+          desc="Pramuka Garuda Bali"
+        />
+
+        {/* borderline */}
+
+        <ItemCarousel
+          textTestimony="“ Wah, pokoknya kalian para garuda wajib banget gabung ATAS. Aku ikut seminar luar negeri tahun 2022 karena dapat informasi dari temen-temen ATAS. ”"
+          imgUser="/images/user1.svg"
+          imgAlt="user1"
+          name="Chandra Dwi Cahya"
+          desc="Pramuka Garuda Jawa Tengah"
+        />
+        <ItemCarousel
+          textTestimony="“ Percaya gak sih, aku lolos masuk IPB karena dibantu dengan ATAS. Penasaran kan kalian? Cepet-cepet gabung deh!. ”"
+          imgUser="/images/user2.svg"
+          imgAlt="user2"
+          name="Kartika Cinta"
+          desc="Pramuka Garuda Jawa Barat"
+        />
+        <ItemCarousel
+          textTestimony="“ Saya gak nyangka banget dapat undangan untuk jadi pembicara di SMP saya dulu hanya karena saya adalah anggota ATAS. ”"
+          imgUser="/images/user3.svg"
+          imgAlt="user3"
+          name="Bli Made"
+          desc="Pramuka Garuda Bali"
+        />
+      </OwlCarousel>
+    </div>
   );
 }
 
