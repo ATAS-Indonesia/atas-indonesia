9b8a5a1307afd3a6231676c82dc1f231c78d71b7
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-12-27 21:19:21 +0700

    feat: update header component

---
+++ src/components/widgets/header/navigation.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+import { useState } from "react";
+import { navigation } from "./constants";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+export function Navigation() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav
+        aria-label="Global"
+        className="flex items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              alt=""
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              className="h-8 w-auto"
+            />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <MenuIcon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map(item => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm/6 font-semibold text-gray-900"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Cek Nomor ATAS <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Sheet open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
+        <SheetTrigger>
+          <div className="fixed inset-0 z-50 lg:hidden" />
+        </SheetTrigger>
+        <SheetContent>
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map(item => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Cek Nomor ATAS
+                </a>
+              </div>
+            </div>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-import Footer from "@/components/footer";
-import Dashboard from "@/components/dashboard";
 import SEO from "@/components/SEO";
+import { Header } from "@/components/widgets/header";
+import { Blog } from "@/components/widgets/blog";
+import { Committee } from "@/components/widgets/committee";
+import { Footer } from "@/components/widgets/footer";
 
 export default function Main() {
   return (
     <>
       <SEO />
 
-      <Dashboard />
+      <Header />
+      <Committee />
+      <Blog />
 
       <Footer />
     </>
