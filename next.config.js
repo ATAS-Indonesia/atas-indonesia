c1e9da26fcd45695176d1f959f4f8299020e9e13
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-12-28 15:14:38 +0700

    chore: modify next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
+    localPatterns: [
+      {
+        pathname: "/images/**",
+        search: "",
+      },
+    ],
     remotePatterns: [
       {
         protocol: "https",
