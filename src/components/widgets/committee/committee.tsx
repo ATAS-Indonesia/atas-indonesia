ba37b55fd0fbdedc001ce1fd8cff5fda1a413c84
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-12-28 15:46:06 +0700

    feat: update pengurus section

--- src/components/widgets/committee/committee.tsx
+++ src/components/widgets/committee/committee.tsx
@@ -1,13 +1,5 @@
-const people = [
-  {
-    name: "Leslie Alexander",
-    role: "Co-Founder / CEO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-];
-
-const person = people[0];
+import Image from "next/image";
+import { people } from "./constants";
 
 export function Committee() {
   return (
@@ -15,21 +7,23 @@ export function Committee() {
       <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
         <div className="max-w-xl">
           <h2 className="text-pretty text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
-            Meet our leadership
+            Pengurus dan Koordinator Wilayah
           </h2>
           <p className="mt-6 text-lg/8 text-gray-600">
-            We’re a dynamic group of individuals who are passionate about what
-            we do and dedicated to delivering the best results for our clients.
+            Pengurus ATAS Indonesia hadir dengan semangat dan dedikasi untuk
+            memajukan pengembangan Pramuka Garuda di seluruh nusantara.
           </p>
         </div>
         <ul className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-          {Array.from({ length: 6 }).map((_, index) => (
+          {people.map((person, index) => (
             <li key={index}>
               <div className="flex items-center gap-x-6">
-                <img
-                  alt=""
+                <Image
+                  alt={person.name}
                   src={person.imageUrl}
                   className="size-16 rounded-full"
+                  width={250}
+                  height={250}
                 />
                 <div>
                   <h3 className="text-base/7 font-semibold tracking-tight text-gray-900">
