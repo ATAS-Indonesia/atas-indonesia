9c8b2e063262a8a8b4b6df7ff47c9e433b15970b
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-12-31 13:45:37 +0700

    feat: add hook get member list

---
+++ src/features/cek-nomor/hooks/types.ts
@@ -0,0 +1,9 @@
+export interface Member {
+  Nomor: string;
+  NSO: string;
+  Golongan: string;
+  "Nomor ATAS": string;
+  "Nama Lengkap": string;
+  "Kwartir Cabang": string;
+  "Kwartir Daerah": string;
+}
---
+++ src/features/cek-nomor/hooks/useFetchMember.ts
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { parseCSV } from "@/utils/data";
+
+export interface Member {
+  Golongan: string;
+  "Kwartir Cabang": string;
+  "Kwartir Daerah": string;
+  NSO: string;
+  "Nama Lengkap": string;
+  Nomor: string;
+  "Nomor ATAS": string;
+}
+
+const GOOGLE_SHEET_LINK =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR31M811KmpqtBdUVWnl6lfyM5kPIXAKtfDEJ4hQWmiUw60z1yox5ibtmGopfRPXjS0Gt38lZ-wsNHZ/pub?gid=17432309&single=true&output=csv";
+
+export const useFetchMember = () => {
+  const memberList = useQuery({
+    queryKey: ["queryMemberList"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(GOOGLE_SHEET_LINK);
+
+        const data = response.data;
+
+        if (data) {
+          return parseCSV(data);
+        }
+
+        return [];
+      } catch (e) {
+        console.error(e);
+        return [];
+      }
+    },
+    enabled: true,
+  });
+
+  return memberList;
+};
